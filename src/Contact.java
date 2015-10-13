c70d68754570316283b7a21de435918be9b48504
oakes <zsoakes@gmail.com>
2015-10-13 10:21:31 -0400

    Add test code to practice constructors and data structures

--- src/Contact.java
+++ src/Contact.java
@@ -7,27 +7,8 @@ public class Contact {
     public String email;
     public int age;
 
-    public Contact() {
-        this.name = "Zach Oakes";
-        this.email = "zach@theironyard.com";
-    }
-
-    public Contact(String name, String address, String email, int age) {
+    public Contact(String name, int age) {
         this.name = name;
-        this.address = address;
-        this.email = email;
         this.age = age;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String newName) {
-        this.name = newName;
-    }
-
-    public static boolean isValidName(String name) {
-        return name.length() != 0;
     }
 }
