cccabe07ead13ab37b59508e6552ab76b1a0ec15
oakes <zsoakes@gmail.com>
2015-11-02 10:27:16 -0500

    Add Exercise 8

---
+++ src/Exercise8.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * Created by zach on 11/2/15.
+ */
+public class Exercise8 {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> names = new ArrayList();
+        Scanner scanner = new Scanner(new File("people.csv"));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            // split line into array of columns
+            String[] columns = line.split(",");
+            // add first and last name to "names"
+            names.add(columns[1] + " " + columns[2]);
+        }
+
+        String searchTerm = "ali";
+        ArrayList<String> results = new ArrayList();
+        // fill up "results" with all the names that contain searchTerm (case-insensitive)
+        searchTerm = searchTerm.toLowerCase();
+        for (String name : names) {
+            if (name.toLowerCase().contains(searchTerm)) {
+                results.add(name);
+            }
+        }
+        System.out.println(results);
+
+        // use streams
+        final String searchTerm2 = searchTerm;
+        List<String> results2 =
+                names
+                .parallelStream()
+                .filter((name) -> {
+                    return name.toLowerCase().contains(searchTerm2);
+                })
+                .collect(Collectors.toList());
+        System.out.println(results2);
+    }
+}
