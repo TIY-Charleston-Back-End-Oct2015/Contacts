2414aa087f7322c5f86f5961ae1bd808e350f1c7
oakes <zsoakes@gmail.com>
2015-11-11 10:09:55 -0500

    Add Exercise 11

---
+++ src/Exercise11.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zach on 11/11/15.
+ */
+public class Exercise11 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Hello")); // false
+        System.out.println(isPalindrome("racecar")); // true
+    }
+
+    static boolean isPalindrome(String s) {
+        return false;
+    }
+}
